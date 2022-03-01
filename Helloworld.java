82c211cce3e5fd672c0f49324effb760ac2d5117
Anju-Chandran01 <86476386+Anju-Chandran01@users.noreply.github.com>
2022-03-01 09:00:43 +0530

    Update : Achange on one of the greeting message

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ package javasamples;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld this is java");
+        System.out.println("HelloWorld");//change on sameline through gitrepo
         System.out.println("HelloWorld this is java");//changes made in local
         System.out.println("HelloWorld this is java");//changes made in gitrepo
     }
