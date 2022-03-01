6f01f13a0531a05ab0d985c343d362f40dfc7fd9
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-01 11:40:49 +0530

    Update : to create a conflict

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");//change on sameline through local
         System.out.println("HelloWorld this is java");//changes made in local
-        System.out.println("HelloWorld");//changes made in gitrepo
+        System.out.println("HelloWorld...... Its java");//changes made in gitrepo
     }
 }
