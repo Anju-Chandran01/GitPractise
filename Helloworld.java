b2661c12babeabe6db642ebdcdca501b99ded92c
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-01 14:42:04 +0530

    Update : creating a conflict

--- Helloworld.java
+++ Helloworld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");//change on sameline through local
         System.out.println("HelloWorld this is java");//changes made in local
-        System.out.println("HelloWorld...... Its java");//changes made in gitrepo
+        System.out.println("Java Program");//changes made in gitrepo
         System.out.println("Hello..");//changes made in gitrepo
     }
 }
